7ef40881f26f3b357fa0716421e961119e1bfb3f
kers0nec <jaden2solid@hotmail.com>
2026-09-23 23:34:50 -0400

    Upgrade project to 67 Obfuscator

--- src/transforms/strings.js
+++ src/transforms/strings.js
@@ -1,13 +1,28 @@
 import crypto from "node:crypto";
 
 export function encodeString(value) {
-  const key = crypto.randomBytes(1)[0] || 173;
-  const salt = crypto.randomBytes(1)[0] || 91;
+  const key1 = crypto.randomBytes(1)[0] || 0x5b;
+  const key2 = crypto.randomBytes(1)[0] || 0xa7;
+  const salt = crypto.randomBytes(1)[0] || 0x31;
   const bytes = Buffer.from(value, "utf8");
-  const encoded = [...bytes].map((b, i) => ((b ^ key) + salt + i) & 255);
-  return { encoded, key, salt };
+  const encoded = bytes.map((b, i) => {
+    const rotated = ((b << (i % 5)) | (b >>> (8 - (i % 5)))) & 255;
+    return (((rotated ^ key1) + salt + i * 13) ^ key2) & 255;
+  });
+  return { encoded: [...encoded], key1, key2, salt };
 }
 
 export function makeDecoderLua() {
-  return `local function _0xD(t,k,s)local r={} for i=1,#t do r[i]=string.char(((t[i]-s-i+1)%256)~k) end return table.concat(r) end`;
+  return [
+    "local function _0x67s(t,a,b,c)",
+    "local r={}",
+    "for i=1,#t do",
+    "local x=((t[i]~b)-c-(i-1)*13)%256",
+    "local n=i%5",
+    "x=((x>>n)|((x<<(8-n))&255))&255",
+    "r[i]=string.char(x~a)",
+    "end",
+    "return table.concat(r)",
+    "end"
+  ].join("");
 }
