a9aabcc12833ceb1946196ef1af655b84422a106
kers0nec <jaden2solid@hotmail.com>
2026-09-23 23:32:04 -0400

    Wire strong obfuscation into compiler and CLI

--- src/compiler.js
+++ src/compiler.js
@@ -1,16 +1,17 @@
 import { parseLua } from "./parser.js";
 import { analyzeScopes } from "./analysis/scopes.js";
 import { renameSymbols } from "./transforms/rename.js";
+import { emit } from "./emitter.js";
 
 export function compile(source, options = {}) {
   const ast = parseLua(source);
   const analysis = analyzeScopes(ast);
   if (options.rename !== false) renameSymbols(analysis.symbols);
-
-  return {
-    ast,
-    analysis,
-    source,
-    options
-  };
+  const output = emit(source, ast, analysis, {
+    rename: options.rename !== false,
+    strings: options.strings !== false,
+    constants: options.constants !== false,
+    stripComments: options.stripComments !== false
+  });
+  return { ast, analysis, source: output, output, options };
 }
