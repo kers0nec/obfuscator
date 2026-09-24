589f7b0ae0fb5d0bdd5b85d9639c8bc9432233e4
kers0nec <jaden2solid@hotmail.com>
2026-09-23 23:34:52 -0400

    Upgrade project to 67 Obfuscator

--- src/transforms/constants.js
+++ src/transforms/constants.js
@@ -1,7 +1,9 @@
 import crypto from "node:crypto";
 
 export function encodeNumber(n) {
-  if (!Number.isInteger(n) || n < -2147483648 || n > 2147483647) return String(n);
+  if (!Number.isFinite(n) || !Number.isInteger(n) || n < -2147483648 || n > 2147483647) {
+    return String(n);
+  }
   const key = crypto.randomBytes(4).readInt32LE(0);
   return `((${n ^ key})~(${key}))`;
 }
